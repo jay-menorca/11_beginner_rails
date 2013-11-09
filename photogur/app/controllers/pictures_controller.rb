b4941bb0c467b9047776c1d6a9fb1a354ad514d4
jay-menorca <zai_menorca@yahoo.com>
2013-11-09 14:59:35 -0500

    unfinished photogur

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -19,28 +19,34 @@ class PicturesController < ApplicationController
 		]
 	end
 
-	def picture0
-		@picture = {
+	def show
+		@pictures = [
+			{
 				:title => "The old church on the coast of white sea",
 				:artist => "Sergey Ershov",
-				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"			
-		}
-	end
-
-	def picture1
-		@picture = {
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+			}, 
+			{
 				:title => "Sea Power",
 				:artist => "Stephen Scullion",
 				:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"				
-			}		
-	end
-
-	def picture2
-		@picture = {
+			},
+			{
 				:title => "Into the Poppies",
 				:artist => "John Wilhelm",
 				:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
 			}
+		]
+
+		@picture = @pictures[params[:id].to_i]		
+	end
+
+	def new
+		
+	end
+
+	def create
+		render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
 	end
 
 end
